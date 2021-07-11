20ea3cabbbe3ff8388697891a78084dc1118e5d2
Nils Kriedner <nkriedner@googlemail.com>
2021-07-11 20:36:58 +0200

    Create Phrase class with constructor and addPhraseToDisplay function

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    addPhraseToDisplay(phrase) {
+        // Grab hold of the place where to put the li's
+        const ul = document.querySelector("#phrase ul");
+        // Create letter array from phrase string:
+        const phraseArray = phrase.split("");
+        // Create an li for each letter:
+        phraseArray.forEach(function (letter) {
+            console.log(letter);
+            const li = document.createElement("li");
+            // If letter is a space:
+            if (letter === " ") {
+                li.className = "space";
+            } else {
+                li.classList.add("hide", "letter", letter);
+            }
+            // Append the li to the ul:
+            ul.appendChild(li);
+        });
+    }
+}
