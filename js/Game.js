d8af40cd63af9dfd1bb85d3bb91af1cbaec5a742
Nils Kriedner <nkriedner@googlemail.com>
2021-07-12 16:02:47 +0200

    Create the Game class with constructor plus startGame() and getRandomPhrase() functions

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,30 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            "We are all one",
+            "I see the divine in you",
+            "How about now",
+            "Surrendering liberates",
+            "Everything is possible",
+        ];
+        this.activePhrase = null;
+    }
+    startGame() {
+        // Hide the start screen overlay
+        document.getElementById("overlay").style.display = "none";
+        // Set the activePhrase to a random phrase:
+        this.activePhrase = getRandomPhrase();
+        // Add phrase to board:
+        // -> Call addPhraseToDisplay() on the activePhrase property
+        phrase.addPhraseToDisplay(this.activePhrase);
+    }
+    getRandomPhrase() {
+        // Create a random number between 1 and the length of the phrase:
+        const randomNumber = Math.floor(
+            Math.random() * this.phrases.length + 1
+        );
+        // Return a phrase based on the random number:
+        return this.phrases[randomNumber];
+    }
+}
