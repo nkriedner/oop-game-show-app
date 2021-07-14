ca7bff3f87d21d1b1e41cba562a9488401348743
Nils Kriedner <nkriedner@googlemail.com>
2021-07-14 16:54:39 +0200

    Update app.js to instatiate a new game only once the start game button is clicked

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Create a new instance of the Game class:
-const game = new Game();
+// const game = new Game();
+let game;
 
 // Add click event listener to start game button:
 const startGameBtn = document.getElementById("btn__reset");
 startGameBtn.addEventListener("click", function () {
     console.log("start game button was clicked");
+    game = new Game();
     game.startGame();
 });
 
